e753568c2fcb43ad95c78f237cbf81cd7b28c97f
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2018-11-12 13:38:09 -0500

    fixed worm eyes to local rotation

--- Assets/WormWranglers/Worm/Scripts/WormPupil.cs
+++ Assets/WormWranglers/Worm/Scripts/WormPupil.cs
@@ -10,10 +10,8 @@ namespace WormWranglers.Worm
 
         private void FixedUpdate()
         {
-            float x = Mathf.PerlinNoise(Time.time * 0.5f, 0) * 2f - 1f;
-            float y = Mathf.PerlinNoise(0, Time.time * 0.5f) * 2f - 1f;
-
-            transform.RotateAround(eye.position, Vector3.up, 30f * Time.deltaTime * Mathf.Cos(Time.time));
+            Vector3 verticalaxis = transform.TransformDirection(Vector3.up);
+            transform.RotateAround(eye.position, verticalaxis, 30f * Time.deltaTime * Mathf.Cos(Time.time));
         }
     }
 }
