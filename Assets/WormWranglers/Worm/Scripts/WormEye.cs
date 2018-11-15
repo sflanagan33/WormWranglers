7bbabb43111f87e76c402b67256f09c30d9457cb
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2018-11-15 15:45:56 -0500

    tweaked models

---
+++ Assets/WormWranglers/Worm/Scripts/WormEye.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WormWranglers.Worm
+{
+    public class WormEye : MonoBehaviour
+    {
+        public float rotationClamp;
+        public float rotationRate;
+        private float value;
+        private bool dir;
+        private Quaternion initial;
+
+        private void Awake()
+        {
+            initial = transform.rotation;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 verticalaxis = transform.TransformDirection(Vector3.up);
+
+            // update value
+            if (dir)
+                value += rotationRate * Time.deltaTime;
+            else
+                value -= rotationRate * Time.deltaTime;
+            // update direction
+            if (value > rotationClamp)
+            {
+                value = rotationClamp;
+                dir = dir == true ? false : true;
+            }
+            if (value < -rotationClamp)
+            {
+                value = -rotationClamp;
+                dir = dir == true ? false : true;
+            }
+            // reset rotation and apply value
+            transform.localRotation = initial;
+            transform.Rotate(verticalaxis, value);
+        }
+    }
+}
